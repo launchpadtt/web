e039f9fcd05e9e11565b4a8969dca810b7485665
Christopher Yee Mon <chrisyeem@protonmail.com>
2022-05-19 00:04:38 -0700

    Udemy: Typescript The Complete Developers Guide Section 11 Chapter 152

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import { User } from './models/User';
+
 console.log('Hi there');
+
+const user = new User({ name: 'myname', age: 20});
+
+user.set({name: 'newname'});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.on('change', () => { 
+  console.log("Change #1");
+});
+user.on('change', () => { 
+  console.log("Change #2");
+});
+user.on('save', () => { 
+  console.log('Save was triggered');
+});
+
+user.trigger('change');
+user.trigger('save');
