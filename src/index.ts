28152843fe100441238e79cfa0704e56de419e9f
Christopher Yee Mon <chrisyeem@protonmail.com>
2022-05-22 10:39:59 -0700

    Udemy Typescript Complete Developers Guide Section 12 Chapter 156 Saving Data

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,47 @@
 import { User } from './models/User';
+import axios from 'axios';
 
-console.log('Hi there');
+// console.log('Hi there');
 
-const user = new User({ name: 'myname', age: 20});
+// const user = new User({ name: 'myname', age: 20});
 
-user.set({name: 'newname'});
+// user.set({name: 'newname'});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+// console.log(user.get('name'));
+// console.log(user.get('age'));
 
-user.on('change', () => { 
-  console.log("Change #1");
-});
-user.on('change', () => { 
-  console.log("Change #2");
-});
-user.on('save', () => { 
-  console.log('Save was triggered');
-});
+// user.on('change', () => { 
+//   console.log("Change #1");
+// });
+// user.on('change', () => { 
+//   console.log("Change #2");
+// });
+// user.on('save', () => { 
+//   console.log('Save was triggered');
+// });
 
-user.trigger('change');
-user.trigger('save');
+// user.trigger('change');
+// user.trigger('save');
+
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 20
+// });
+
+// axios.get('http://localhost:3000/users/1');
+
+// const user = new User({ id: 1});
+
+// user.fetch();
+
+// setTimeout(() => {
+  // console.log(user);
+// }, 4000);
+
+// const user = new User ({ id: 1 });
+
+// user.set({ name: 'NEW NAME', age: 9999 });
+
+const user = new User ({ name: "new record" });
+
+user.save();
